447104c06e57932ed3b108124ba0a66a0b33a924
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-10 08:53:11 +0800

    added the encoder model

--- visual_model.py
+++ visual_model.py
@@ -1,6 +1,59 @@
 import torch
 import torch.nn as nn
 
+
+class GeometricEncoder(nn.Module):
+    def __init__(self,input_nc=3,z_dim=64,bottom=False):
+        super().__init__()
+        self.bottom = bottom
+
+        if self.bottom:
+            self.enc_down_0 = nn.Sequential([
+                nn.Conv2d(input_nc + 4,z_dim,3,stride=1,padding=1),
+                nn.ReLU(True)])
+        self.enc_down_1 = nn.Sequential(nn.Conv2d(z_dim if bottom else input_nc+4, z_dim, 3, stride=2 if bottom else 1,  padding=1),
+                                        nn.ReLU(True))
+        self.enc_down_2 = nn.Sequential(nn.Conv2d(z_dim, z_dim, 3, stride=2, padding=1),
+                                        nn.ReLU(True))
+        self.enc_down_3 = nn.Sequential(nn.Conv2d(z_dim, z_dim, 3, stride=2, padding=1),
+                                        nn.ReLU(True))
+        self.enc_up_3 = nn.Sequential(nn.Conv2d(z_dim, z_dim, 3, stride=1, padding=1),
+                                      nn.ReLU(True),
+                                      nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False))
+        self.enc_up_2 = nn.Sequential(nn.Conv2d(z_dim*2, z_dim, 3, stride=1, padding=1),
+                                      nn.ReLU(True),
+                                      nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False))
+        self.enc_up_1 = nn.Sequential(nn.Conv2d(z_dim * 2, z_dim, 3, stride=1, padding=1),
+                                      nn.ReLU(True))
+
+    def forward(self,x):
+        """
+        input:
+            x: input image, [B,3,H,W]
+        output:
+            feature_map: [B,C,H,W]
+        """
+        W,H = x.shape[3], x.shape[2]
+        X = torch.linspace(-1,1,W)
+        Y = torch.linspace(-1,1,H)
+        y1_m,x1_m = torch.meshgrid([Y,X])
+        x2_m,y2_m = 2 - x1_m,2 - y1_m # Normalized distance in the four direction
+        pixel_emb = torch.stack([x1_m,x2_m,y1_m,y2_m]).to(x.device).unsqueeze(0) # [1,4,H,W]
+        pixel_emb = pixel_emb.repeat([x.size(0),1,1,1])
+        inputs = torch.cat([x,pixel_emb],dim=1)
+
+        if self.bottom:
+            x_down_0 = self.enc_down_0(inputs)
+            x_down_1 = self.enc_down_1(x_down_0)
+        else:
+            x_down_1 = self.enc_down_1(inputs)
+        x_down_2 = self.enc_down_2(x_down_1)
+        x_down_3 = self.enc_down_3(x_down_2)
+        x_up_3 = self.enc_up_3(x_down_3)
+        x_up_2 = self.enc_up_2(torch.cat([x_up_3, x_down_2], dim=1))
+        feature_map = self.enc_up_1(torch.cat([x_up_2, x_down_1], dim=1))  # BxCxHxW
+        return feature_map
+
 class FeatureDecoder(nn.Module):
     def __init__(self, inchannel,input_channel,object_dim = 100):
         super(FeatureDecoder, self).__init__()
