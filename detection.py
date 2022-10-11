a8cf623059700c7ff68beb0b1870302d5e8cea10
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-12 00:38:39 +0800

    Update detection.py

--- detection.py
+++ detection.py
@@ -2,3 +2,13 @@ import torch
 import torch.nn as nn
 
 import cv2
+
+
+def parse_raw_lines(image):
+    return 0
+
+def parse_raw_circles(image):
+    return 0
+
+def parse_raw_points(image):
+    return 0
