512b99c8d74a2cbb99b61b5147ad805ee886d853
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-13 06:27:30 +0800

    Update lcnet.py

--- lcnet.py
+++ lcnet.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 from torch_geometric.data import Data
 from torch_geometric.nn import MessagePassing
 
+import networkx as nx
+
 from config import *
 
 def same_circle(a,b):return False
@@ -13,6 +15,9 @@ def same_line(a,b):return False
 class LCNet(MessagePassing):
     def __init__(self,opt = model_opt):
         super().__init__()
+        self.line_count   = 0
+        self.circle_count = 0
+        self.point_count  = 0
 
         # record features from the input lines and circles
         self.line_features = {}
@@ -20,8 +25,9 @@ class LCNet(MessagePassing):
         # after the propagation
 
         # construct the crude rpresentation of input lines and circles
-        self.lines = {}
+        self.lines   = {}
         self.circles = {}
+        self.points  = {}
 
         # the name of the points will be generated automatically
 
@@ -30,20 +36,23 @@ class LCNet(MessagePassing):
     def build_lc(self,lines,circles):
         self.line_count   = 0
         self.circle_count = 0
+        self.point_count  = 0
+    
         for line in lines:
             flag = True
             for k in self.lines:
                 if same_line(self.lines[k],line) and flag:flag = False
             if flag:
                 self.line_count += 1
-                self.lines["p{}".format(self.line_count)] = line
+                self.lines["l{}".format(self.line_count)] = line
         for circle in circles:
             flag = True
             for k in self.circles:
                 if same_circle(self.circles[k],circle) and flag:flag = False
             if flag:
                 self.circle_count += 1
-                self.lines["p{}".format(self.circle_count)] = circle
+                self.lines["c{}".format(self.circle_count)] = circle
+
         return 0
 
     def realize_lc(self):
