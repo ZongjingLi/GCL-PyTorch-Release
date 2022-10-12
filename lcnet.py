1a642084293ae917782ed0bf99385db8b4020fd4
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-13 06:25:25 +0800

    Update lcnet.py

--- lcnet.py
+++ lcnet.py
@@ -6,6 +6,10 @@ from torch_geometric.nn import MessagePassing
 
 from config import *
 
+def same_circle(a,b):return False
+
+def same_line(a,b):return False
+
 class LCNet(MessagePassing):
     def __init__(self,opt = model_opt):
         super().__init__()
@@ -16,7 +20,7 @@ class LCNet(MessagePassing):
         # after the propagation
 
         # construct the crude rpresentation of input lines and circles
-        self.line = {}
+        self.lines = {}
         self.circles = {}
 
         # the name of the points will be generated automatically
@@ -24,6 +28,22 @@ class LCNet(MessagePassing):
     def forward(self,x):return 0
 
     def build_lc(self,lines,circles):
+        self.line_count   = 0
+        self.circle_count = 0
+        for line in lines:
+            flag = True
+            for k in self.lines:
+                if same_line(self.lines[k],line) and flag:flag = False
+            if flag:
+                self.line_count += 1
+                self.lines["p{}".format(self.line_count)] = line
+        for circle in circles:
+            flag = True
+            for k in self.circles:
+                if same_circle(self.circles[k],circle) and flag:flag = False
+            if flag:
+                self.circle_count += 1
+                self.lines["p{}".format(self.circle_count)] = circle
         return 0
 
     def realize_lc(self):
