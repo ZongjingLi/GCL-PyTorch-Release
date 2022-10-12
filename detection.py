8fc55f397e8ebd1dbe77c6e0533cd6c00b339dc6
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-13 03:40:48 +0800

    update

--- detection.py
+++ detection.py
@@ -97,6 +97,59 @@ def reveal_all_constraints(names,split = "train"):
 cons = ["ccc","cccc","cccl","ccl","cct","clcc","clcl","cll","clll","clt","lcc",
 "llcc","lll","lllc","llll","llt","tcc","tcl","tll"]
 
-#reveal_all_constraints(cons)
+def detect_lines_and_circles(image_path):
+    img = cv2.imread(image_path,cv2.IMREAD_COLOR)
+    # convert to gray scale
 
-reveal_all_concepts(names)
+    gray_blurred = cv2.cvtColor(img ,cv2.COLOR_RGBA2GRAY)
+    # blur using the 3x3 kernel
+
+    #gray_blurred = cv2.medianBlur(gray_blurred,3)
+
+    detected_circles = cv2.HoughCircles(gray_blurred, 
+                   cv2.HOUGH_GRADIENT, 1, 60, param1 = 80,param2 = 30, minRadius = 1, maxRadius = 155)
+    #cv2.HoughCircles(gray_blurred,
+                                    #cv2.HOUGH_GRADIENT,1,55,param1 = 70,
+                                    #param2 = 30, minRadius = 5, maxRadius = 150)
+
+
+    edges = cv2.Canny(gray_blurred,25, 50)
+    detected_lines= cv2.HoughLinesP(edges, 1, np.pi/180, 60, maxLineGap=150)
+    #detected_lines= cv2.HoughLines(gray_blurred, 1, np.pi/180,700)
+
+    output_lines = []
+
+    if detected_lines is not None:
+        for line in detected_lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(img, (x1, y1), (x2, y2), (0, 0, 256), 2)
+            output_lines.append([(x1,y1),(x2,y2)])
+
+    #cv2.imshow("Detected Lines",img)
+    #cv2.waitKey(0)
+
+    output_circles = []
+
+    # draw circles that are detected
+    if detected_circles is not None:
+
+        # convert the circle parameters a,b and r to integers
+        detected_circles = np.uint16(np.around(detected_circles))
+
+        # namomo, adjust lines
+        for pt in detected_circles[0, :]:
+            a, b, r = pt[0], pt[1], pt[2]
+  
+            # Draw the circumference of the circle.
+            cv2.circle(img, (a, b), r, (0, 255, 0), 3)
+  
+            # Draw a small circle (of radius 1) to show the center.
+            cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
+            output_circles.append([a,b,r])
+    
+    return output_lines,output_circles
+
+#reveal_all_constraints(cons)
+if __name__ == "__main__":
+    l,c = detect_lines_and_circles("geoclidean/constraints/concept_lll/train/3_fin.png")
+    #reveal_all_concepts(names)
--- train.py
+++ train.py
@@ -3,12 +3,37 @@ import torch.nn as nn
 
 import argparse
 
+from datasets import *
+from detection import *
+from primitives import *
+from config import *
+
 train_parser = argparse.ArgumentParser()
 train_parser.add_argument("--epoch", default = 9999, type = int, help = "the default trainingepoch for the model")
 train_parser.add_argument("--lr", default = 2e-4, type = float, help = "the learning rate for the optimizer")
 train_parser.add_argument("--mode", default = "Mode", type = str, help = "the training mode for the geometric concept")
-
+train_parser.add_argument("--checkpoint_path", default = None, help = "the location of the check point")
 train_config = train_parser.parse_args(args = [])
 
+BCELoss = torch.nn.BCELoss(reduction = "mean")
+
+model = GeometricConstructor()
+
+dataset = []
+train_loader = DataLoader(dataset, batch_size = 1, shuffle = True)
+
+optimizer = torch.optim.Adam(model.parameters(), lr = 2e-4)
+
 for epoch in range(train_config.epoch):
-    pass
+    total_loss = 0
+    optimizer.zero_grad()
+    for sample in train_loader:
+        concept,image = sample["concept"],sample["image"]
+
+        model.build_dag(concept)
+        model.realize()
+
+        lines,circles = detect_lines_and_circles(image)
+
+    total_loss.backward()
+    optimizer.step()
