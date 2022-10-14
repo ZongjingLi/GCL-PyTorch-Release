57c6cd91f18c23bd1e04cbe324a7d59a0c86660a
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-14 09:03:05 +0800

    Update lcnet.py

--- lcnet.py
+++ lcnet.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from torch_geometric.data import Data
-import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
+
+from moic.mklearn.nn.functional_net import FCBlock
 
 import networkx as nx
 
@@ -28,45 +30,63 @@ class LCNet(nn.Module):
         # construct the crude rpresentation of input lines and circles
         self.lines   = {}
         self.circles = {}
-        self.points  = {}
 
         # the name of the points will be generated automatically
         self.graph = nx.DiGraph()
 
         self.net_data = 0
         
-        self.conv1 = GCNConv(132, 16)
-        self.conv2 = GCNConv(16, 10)
+        # the embeeding 
+        self.conv1  =  GCNConv(64, 32)
+        self.conv2  =  GCNConv(32, 64)
+        self.conv3  =  GCNConv(64, 32)
+        # use the graph convolution to map the data from the point and circle
+        self.line_mapper    = FCBlock(132,3,4,64)
+        self.circle_mapper  = FCBlock(132,3,3,64)
 
     def forward(self,data):
         x, edge_index = data.x, data.edge_index
 
         x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
+        x = F.celu(x)
+        x = F.dropout(x,training = True)
 
-        return F.log_softmax(x, dim=1)
+        x = self.conv2(x, edge_index)
+        x = F.celu(x)
+        x = F.dropout(x,training = True)
+        
+        x = self.conv3(x,edge_index)
+
+        return F.celu(x)
 
     def build_dag_lc(self,lines,circles):
         self.line_count   = 0
         self.circle_count = 0
         self.point_count  = 0
-    
+
+        x = []
+        
+        # just don't detect whether input segments are the same for a while.
+        # the version is crude, but is should work.
         for line in lines:
             flag = False # the flag for is there is at least one line the same line with the new line
             for k in self.lines:
                 if not flag and same_line(self.lines[k],line):flag = True # for each line deteced, if it is a new line, add it into the diction.
-            if not flag:self.line_count += 1;self.lines["l{}".format(self.line_count)] = line
+            if not flag:
+                self.line_count += 1;self.lines["l{}".format(self.line_count)] = line
+                line_feature    =  self.line_mapper(line);x.append(line_feature)
+                self.line_embeddings["l{}".format(self.line_count)] = line_feature
         for circle in circles:
             flag = False # the flag for is there is at least one circle the same circle with the new circle
             for k in self.circles:
                 if not flag and same_circle(self.circles[k],circle):flag = True # for each circle detected, if it is a new circle, added into the diction.
-            if not flag:self.circle_count += 1;self.lines["c{}".format(self.circle_count)] = circle
-    
-        x = []
+            if not flag:
+                self.circle_count += 1;self.circles["c{}".format(self.circle_count)] = circle
+                circle_feature    =  self.circle_mapper(circle);x.append(circle_feature)
+                self.circle_embeddings["c{}".format(self.circle_count)] = circle_feature
+        x = torch.cat(x,-1)
         edges = []
-        net_data = Data(x,edges)
+        self.net_data = Data(x = x,edges = edges)
         return 0
 
     def realize_lc(self):
