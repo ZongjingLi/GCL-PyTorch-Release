7ba630fa3443b1e3c15ce550b13684d1b2691836
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-20 10:51:40 +0800

    Update structure_search.py

--- structure_search.py
+++ structure_search.py
@@ -3,3 +3,21 @@ import torch.nn as nn
 
 import numpy as np
 import networkx as nx
+
+class ConceptSearch(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.heuistics = None
+
+        self.best_concepts = []
+
+    def get_node(self,node):
+        return node
+    
+    def search(self,image):
+        return 0
+
+if __name__ == "__main__":
+    astar_search = ConceptSearch()
+
+    
